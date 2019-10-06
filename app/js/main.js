85c3da68cc01ed5b3e8d82c81a8322e6bdbd2d9b
Ravshan014 <51761087+Ravshan014@users.noreply.github.com>
2019-10-07 01:12:23 +0500

    Добавил полный свёрстанный, адаптивный сайт
    Сайт про Белаз

---
+++ app/js/main.js
@@ -0,0 +1,36 @@
+
+	$img = $('.phone-xs__img');	
+	$phone = $('.phone-number-xs');
+	$navbar = $('.navbar');
+
+	$img.on('click', function() {
+	  $phone.toggleClass('opened');
+	  $navbar.toggleClass('is-opened');
+	});
+
+
+
+	$('.slider-sliders').slick({
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	autoplay: true,
+	autoplaySpeed: 2000,
+	});
+
+
+
+
+
+
+
+
+
+
+	
+
+	
+
+
+
+
+	
